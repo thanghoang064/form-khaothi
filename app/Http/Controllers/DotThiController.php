0495c888e3ea279a6175a1ae2b2f782d4f2f7269
haudv3202 <haudang02.it@gmail.com>
2022-10-26 22:08:30 +0700

    hau them sua xoa ki hoc

--- app/Http/Controllers/DotThiController.php
+++ app/Http/Controllers/DotThiController.php
@@ -101,25 +101,6 @@ class DotThiController extends Controller
         }
     }
 
-    public function index_ky_hoc(){
-        return view('admin.kyhoc.index');
-    }
 
-    public function add_ky_hoc(){
-        return view('admin.kyhoc.add_ky_hoc');
-    }
-
-    public function new_ky_hoc(request $request){
-        $request->validate(
-            ['name_ky_hoc'=> 'required|min:6',
-            ],
-            [
-                'required' => 'Không để trống tên kỳ học',
-                'min' => 'Chiều dài tối thiểu :min kí tự'
-            ]
-        );
-
-
-    }
 
 }
