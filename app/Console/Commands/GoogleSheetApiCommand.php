8227e3dd83b7404098a9fbbd26597d9db20c1e47
ITPLUS Academy <thanghoang064@gmail.com>
2022-12-06 14:28:33 +0700

    update source

--- app/Console/Commands/GoogleSheetApiCommand.php
+++ app/Console/Commands/GoogleSheetApiCommand.php
@@ -42,9 +42,10 @@ class GoogleSheetApiCommand extends Command
         Log::debug('start update sheet 1 data');
         $client = $this->getGooogleClient();
 
-        $service = new Google_Service_Sheets($client);
-        Log::debug("hello coto11");
         $dataDotThi = DB::table('dot_thi')->where('status','=',1)->first();
+        Log::debug("hello coto11");
+        $service = new Google_Service_Sheets($client);
+
         Log::debug("hello coto");
         if (!empty($dataDotThi) ) {
 //            $spreadsheetId = '1yOqz4qAmKXTPtKbXYA-fTKK7-z6CNTCNaZmNoYJ48rg';
