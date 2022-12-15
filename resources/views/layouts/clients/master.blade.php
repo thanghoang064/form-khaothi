39b7a6eff647a30273fd55c58198a05def8fcf1b
LittleNora <vinhndqph26105@fpt.edu.vn>
2022-12-15 15:30:35 +0700

    vinhndq tối ưu báo cáo thi

--- resources/views/layouts/clients/master.blade.php
+++ resources/views/layouts/clients/master.blade.php
@@ -9,6 +9,7 @@
     <link rel="shortcut icon" href="{{asset('metronic')}}/media/logos/favicon.ico" />
     <link rel="stylesheet" href="{{asset('vendor/bootstrap/css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('styles/client-layout.css')}}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://kit.fontawesome.com/29b41ee1c8.js" crossorigin="anonymous"></script>
     @yield('style-custom')
 </head>
@@ -54,7 +55,6 @@
     </nav>
     @yield('content')
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 @yield('page-script')
